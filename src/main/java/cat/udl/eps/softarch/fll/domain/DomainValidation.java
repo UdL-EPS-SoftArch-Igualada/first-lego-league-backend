f418645e8a7e3c336f3c983b67b3b23d89645fed
Alex Lillo Lindo <alillolindo@gmail.com>
2026-03-07 16:04:23 +0100

    fix: fixes

--- src/main/java/cat/udl/eps/softarch/fll/domain/DomainValidation.java
+++ src/main/java/cat/udl/eps/softarch/fll/domain/DomainValidation.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public final class DomainValidation {
 
 	private static final Pattern EMAIL_PATTERN =
-		Pattern.compile("^[A-Za-z0-9+_.%-]++@(?:[A-Za-z0-9-]++\\.)+[A-Za-z]{2,}+$");
+		Pattern.compile("^[A-Za-z0-9+_.%-]++@[A-Za-z0-9-]++\\.(?:[A-Za-z0-9-]++\\.)*+[A-Za-z]{2,}+$");
 
 	private DomainValidation() {
 	}
